bd80630584df8e26741d0c636a620a59f57b913d
Ronald-br <ronyvieira115@gmail.com>
2026-08-06 17:54:13 -0300

    Adiciona criptografia das notas e README do projeto

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
 
 class Note extends Model
 {
@@ -16,6 +17,17 @@ class Note extends Model
         'user_id',
     ];
 
+    // Criptografa automaticamente antes de salvar no banco
+    public function setConteudoAttribute($value)
+    {
+        $this->attributes['conteudo'] = Crypt::encryptString($value);
+    }
+
+    // Descriptografa automaticamente quando buscar do banco
+    public function getConteudoAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 
     public function user()
     {
